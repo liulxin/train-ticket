d95c75c452ccb9af2be66e4508f915296c9f3f18
liulxin <550293874@qq.com>
2020-02-11 11:39:56 +0800

    createContext

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React, { Component, createContext } from "react";
+import "./App.css";
+
+const BatteryContext = createContext(90);
+// const BatteryContext = createContext();
+const OnlineContext = createContext();
+
+class Leaf extends Component {
+  render() {
+    return (
+      <BatteryContext.Consumer>
+        {battery => (
+          <OnlineContext.Consumer>
+            {online => (
+              <h1>
+                battery => {battery}, online => {String(online)}
+              </h1>
+            )}
+          </OnlineContext.Consumer>
+        )}
+      </BatteryContext.Consumer>
+    );
+  }
+}
+
+class Middle extends Component {
+  render() {
+    return <Leaf />;
+  }
+}
+
+class App extends Component {
+  state = {
+    battery: 60,
+    online: false
+  };
+
+  render() {
+    const { battery, online } = this.state;
+
+    return (
+      <BatteryContext.Provider value={battery}>
+        <OnlineContext.Provider value={online}>
+          <button
+            type="button"
+            onClick={() =>
+              this.setState({
+                battery: battery - 1
+              })
+            }
+          >
+            press
+          </button>
+          <button
+            type="button"
+            onClick={() =>
+              this.setState({
+                online: !online
+              })
+            }
+          >
+            change
+          </button>
+          <Middle />
+        </OnlineContext.Provider>
+      </BatteryContext.Provider>
+    );
+  }
+}
+
+export default App;
