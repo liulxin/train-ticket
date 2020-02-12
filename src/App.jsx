5e3f2ed62753793e9a005e5d45ecf125a1dd91b9
liulxin <550293874@qq.com>
2020-02-12 11:09:46 +0800

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
-import React, { Component, useState, useMemo, useCallback, memo } from "react";
+import React, { Component, PureComponent, useState, useEffect, useMemo, useRef, useCallback, memo } from "react";
 
-const Counter = memo(function Counter(props) {
-  console.log("counter render");
-  return <div onClick={props.onClick}>{props.count}</div>;
-});
+// const Counter = memo(function Counter(props) {
+//   console.log("counter render");
+//   return <div onClick={props.onClick}>{props.count}</div>;
+// });
+
+class Counter extends PureComponent {
+  speak() {
+    console.log(`now counter is: ${this.props.count}`)
+  }
+  render() {
+    const {props} = this
+    return <div onClick={props.onClick}>{props.count}</div>;
+  }
+}
 
 function App(props) {
   const [count, setCount] = useState(0);
+  const counterRef = useRef()
+  let it = useRef();
 
   const double = useMemo(() => {
     return count * 2;
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [count === 3]);
 
-  // const onClick = () => {
-  //   console.log('onclick')
-  // }
-
   // useMemo(() => fn)
   // useCallback(fn)
   const onClick = useCallback(() => {
-    console.log("onclick");
-  }, []);
+    counterRef.current.speak()
+  }, [counterRef]);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    it.current = setInterval(() => {
+      setCount(count => count + 1)
+    }, 1000)
+  }, [])
+
+  useEffect(() => {
+    if(count >= 10) {
+      clearInterval(it.current)
+    }
+  })
 
   return (
     <div>
       <div onClick={() => setCount(count + 1)}>
         Click {count}, doublue: {double}
       </div>
-      <Counter count={double} onClick={onClick} />
+      <Counter ref={counterRef} count={double} onClick={onClick} />
     </div>
   );
 }
