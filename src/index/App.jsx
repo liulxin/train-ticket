e33a40bedd7ec978c89b4d885c4d1369be550d60
liulxin <550293874@qq.com>
2020-02-14 14:42:33 +0800

    departDate

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -6,20 +6,32 @@ import DepartDate from "./DepartDate";
 import HighSpeed from "./HighSpeed";
 import Journey from "./Journey";
 import Submit from "./Submit";
-
+import {h0} from '../common/fp'
 import "./App.css";
 import CitySelector from "../common/CitySelector";
+import DateSelector from "../common/DateSelector.jsx";
 
-import { exchangeFromTo, showCitySelector, hideCitySelector, fetchCityData, setSelectedCity } from "./actions";
+import {
+  exchangeFromTo,
+  showCitySelector,
+  hideCitySelector,
+  fetchCityData,
+  setSelectedCity,
+  showDateSelector,
+  hideDateSelector,
+  setDepartDate
+} from "./actions";
 
 function App(props) {
   const {
     from,
     to,
     isCitySelectorVisible,
+    isDateSelectorVisible,
     cityData,
     isLoadingCityData,
-    dispatch
+    dispatch,
+    departDate
   } = props;
 
   const onBack = useCallback(() => {
@@ -38,11 +50,46 @@ function App(props) {
     [dispatch]
   );
 
-  const citySelectorCbs = useMemo(() => bindActionCreators({
-    onBack: hideCitySelector,
-    fetchCityData,
-    onSelect: setSelectedCity
-  }, dispatch), [dispatch])
+  const citySelectorCbs = useMemo(
+    () =>
+      bindActionCreators(
+        {
+          onBack: hideCitySelector,
+          fetchCityData,
+          onSelect: setSelectedCity
+        },
+        dispatch
+      ),
+    [dispatch]
+  );
+
+  const departDateCbs = useMemo(
+    () =>
+      bindActionCreators(
+        {
+          onClick: showDateSelector
+        },
+        dispatch
+      ),
+    [dispatch]
+  );
+
+  const dateSelectorCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        onBack: hideDateSelector
+      },
+      dispatch
+    );
+  }, [dispatch]);
+
+  const onSelectDate = useCallback((day) => {
+    if(!day || (day < h0())) {
+      return
+    }
+    dispatch(setDepartDate(day))
+    dispatch(hideDateSelector())
+  },[dispatch])
 
   return (
     <div>
@@ -51,7 +98,7 @@ function App(props) {
       </div>
       <form className="form">
         <Journey from={from} to={to} {...cbs}></Journey>
-        <DepartDate></DepartDate>
+        <DepartDate time={departDate} {...departDateCbs}></DepartDate>
         <HighSpeed></HighSpeed>
         <Submit></Submit>
       </form>
@@ -60,6 +107,11 @@ function App(props) {
         cityData={cityData}
         isLoading={isLoadingCityData}
         {...citySelectorCbs}
+      />
+      <DateSelector
+        show={isDateSelectorVisible}
+        {...dateSelectorCbs}
+        onSelect= {onSelectDate}
       />
     </div>
   );
