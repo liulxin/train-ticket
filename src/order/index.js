5e519067efe04cdecb7e40976e44718937bc5836
liulxin <550293874@qq.com>
2020-02-20 18:49:56 +0800

    order

--- src/order/index.js
+++ src/order/index.js
@@ -1,15 +1,22 @@
-import React from "react";
-import ReactDom from "react-dom";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import 'normalize.css/normalize.css';
+import * as serviceWorker from '../serviceWorker';
 
-import 'normalize.css/normalize.css'
-import store from "./store";
-import "./index.css";
-import App from "./App.jsx";
+import store from './store';
+import './index.css';
+import App from './App.jsx';
 
-ReactDom.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
+
+if ('production' === process.env.NODE_ENV) {
+    serviceWorker.register();
+} else {
+    serviceWorker.unregister();
+}
